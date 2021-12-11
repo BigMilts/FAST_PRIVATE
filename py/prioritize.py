9c654391e8c0d3ab701e761ba8acaede56b9e35b
BigMilts <miltonvieirasoutomaior09@outlook.com>
2021-12-11 10:48:11 -0300

    testing action write 7

--- py/prioritize.py
+++ py/prioritize.py
@@ -93,14 +93,10 @@ def write_results(results):
     print(os.path)
     if not os.path.exists(directory):
         os.makedirs(directory)
-    repo = Repo.init(directory).git
-    index = Repo.init(directory).index
     file = open("../output/results.txt", 'w')
     for result in results:
         file.write(result)
         file.write("\n")
-    repo.add(file)
-    index.commit("Persisting data")
 
 
 if __name__ == "__main__":
